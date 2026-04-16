35f3915dc2386400d5b62ad9c89ecd3cb1b6bc04
jon-rod21 <jonathan.rodriguez9921@gmail.com>
2026-04-16 16:38:24 -0500

    clients can submit calcs thru user input

--- MathClient.java
+++ MathClient.java
@@ -52,24 +52,24 @@ public class MathClient {
 
             System.out.println("[CLIENT] Server response: " + ack);
 
-            // Step 2: Send CALC requests
-            String[] expressions = {
-                "10 + 25",
-                "100 / 4",
-                "7 * 8",
-                "50 - 13",
-                "2 + 2"
-            };
 
-            Random rand = new Random();
+            System.out.println("\n[CLIENT] You can now send math expressions.\n " + 
+                    "[CLIENT] Supported opearator are: + - * / ()\n" + 
+                    "[CLIENT] Ex: 5 + 3 | (2 + 3 * 7) | 10 / 3 * (2 + 4)\n" + 
+                    "[CLIENT] Type 'quit' to disconnect\n");
 
-            for (int i = 0; i < 3; i++) {  // at least 3 required
-                String expr = expressions[rand.nextInt(expressions.length)];
-
-                int delay = 1000 + rand.nextInt(3000);
-                Thread.sleep(delay);
-
-                String calcMsg = "CALC:" + expr;
+            while (true)
+            {
+                System.out.println("[" + clientName + "] > ");
+                String input = sc.nextLine().trim();
+                if (input.matches("quit"))
+                {
+                    System.out.println("[CLIENT] Sending: QUIT");
+                    out.println("QUIT");
+                    break;
+                }
+                if (input.isEmpty()) continue;
+                String calcMsg = "CALC:" + input;
                 System.out.println("[CLIENT] Sending: " + calcMsg);
                 out.println(calcMsg);
 
@@ -77,9 +77,34 @@ public class MathClient {
                 System.out.println("[CLIENT] Received: " + response);
             }
 
-            // Step 3: QUIT
-            System.out.println("[CLIENT] Sending: QUIT");
-            out.println("QUIT");
+//            // Step 2: Send CALC requests
+//            String[] expressions = {
+//                "10 + 25",
+//                "100 / 4",
+//                "7 * 8",
+//                "50 - 13",
+//                "2 + 2"
+//            };
+//
+//            Random rand = new Random();
+//
+//            for (int i = 0; i < 3; i++) {  // at least 3 required
+//                String expr = expressions[rand.nextInt(expressions.length)];
+//
+//                int delay = 1000 + rand.nextInt(3000);
+//                Thread.sleep(delay);
+//
+//                String calcMsg = "CALC:" + expr;
+//                System.out.println("[CLIENT] Sending: " + calcMsg);
+//                out.println(calcMsg);
+//
+//                String response = in.readLine();
+//                System.out.println("[CLIENT] Received: " + response);
+//            }
+//
+//            // Step 3: QUIT
+//            System.out.println("[CLIENT] Sending: QUIT");
+//            out.println("QUIT");
 
             socket.close();
             System.out.println("[CLIENT] Disconnected.");
